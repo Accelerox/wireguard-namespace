19cc05f3fed19c43ffcdc2f21a9e8f3a933a4533
Max Idermark <max@idermark.com>
2023-03-30 23:07:46 +0200

    Added sudo check

--- vpn-stop.sh
+++ vpn-stop.sh
@@ -5,6 +5,12 @@
 #
 # Usage: ./vpn-stop.sh <identifier>
 
+# Check if the script is run with sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run with sudo"
+    exit 1
+fi
+
 if [[ $# -ne 1 ]]; then
     echo "Usage: $0 <identifier>"
     exit 1
