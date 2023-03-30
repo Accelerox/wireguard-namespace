73f360255adbe1c509bbf259d6807fbc2b1b08fe
Max Idermark <max@idermark.com>
2023-03-30 23:07:28 +0200

    Added sudo check

--- vpn-install.sh
+++ vpn-install.sh
@@ -4,5 +4,11 @@
 #
 # Usage: ./vpn-install.sh
 
+# Check if the script is run with sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run with sudo"
+    exit 1
+fi
+
 # Create bridge br0
 nmcli connection add type bridge ifname br0 stp no
